32ccb97fe92a0b7018394dde1cf262742767db55
aldyvictor <aldyvictor1046@gmail.com>
2022-06-09 21:57:41 +0700

    Integration total item cart to navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -80,7 +80,15 @@
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link d-inline-block mt-2">
-                  <img src="/images/icon-cart-empty.svg" alt="" />
+                    @php
+                        $carts = \App\Models\Cart::where('users_id', Auth::user()->id)->count();
+                    @endphp
+                    @if ($carts > 0)
+                        <img src="/images/icon-cart-filled.svg" alt="" />
+                        <div class="cart-badge">{{ $carts }}</div>
+                    @else
+                        <img src="/images/icon-cart-empty.svg" alt="" />
+                    @endif
                 </a>
               </li>
             </ul>
@@ -88,7 +96,7 @@
             <!-- Mobile Menu -->
             <ul class="navbar-nav d-block d-lg-none">
               <li class="nav-item">
-                <a href="#" class="nav-link"> Hi, Aldy </a>
+                <a href="#" class="nav-link"> Hi, {{ Auth::user()->name }} </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link d-inline-block"> Cart </a>

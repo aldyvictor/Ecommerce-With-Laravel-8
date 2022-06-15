ad44b3c6af4ee78fe3fe87cba1e656df7953b0c2
aldyvictor <aldyvictor1046@gmail.com>
2022-06-15 21:00:30 +0700

    feat(controller): delete carts after checkout by user

--- app/Http/Controllers/CheckoutCtrl.php
+++ app/Http/Controllers/CheckoutCtrl.php
@@ -52,6 +52,9 @@ class CheckoutCtrl extends Controller
             ]);
         }
 
+        // Delete Cart Data
+        Cart::where('users_id', Auth::user()->id)->delete();
+
         // Configuration Midtrans
         Config::$serverKey = config('services.midtrans.serverKey');
         Config::$isProduction = config('services.midtrans.isProduction');

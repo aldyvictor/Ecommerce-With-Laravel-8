a8cb2be074fb2ab6db6a5bf62673c80433832b48
aldyvictor <aldyvictor1046@gmail.com>
2022-06-15 21:07:12 +0700

    Setting middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -14,39 +14,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/categories', [App\Http\Controllers\CategoryCtrl::class, 'index'])->name('categories');
 Route::get('/categories/{id}', [App\Http\Controllers\CategoryCtrl::class, 'detail'])->name('categories-detail');
+
 Route::get('/details/{id}', [App\Http\Controllers\DetailCtrl::class, 'index'])->name('detail');
 Route::post('/details/{id}', [App\Http\Controllers\DetailCtrl::class, 'addToCart'])->name('detail-add');
-Route::get('/cart', [App\Http\Controllers\CartCtrl::class, 'index'])->name('cart');
-Route::delete('/cart/{id}', [App\Http\Controllers\CartCtrl::class, 'delete'])->name('cart-delete');
 
-Route::post('/checkout', [App\Http\Controllers\CheckoutCtrl::class, 'process'])->name('checkout');
 Route::post('/checkout/callback', [App\Http\Controllers\CheckoutCtrl::class, 'callbackCheckout'])->name('callback-checkout');
 
 Route::get('/success', [App\Http\Controllers\CartCtrl::class, 'success'])->name('success');
 
-Route::get('/dashboard', [App\Http\Controllers\DashboardCtrl::class, 'index'])->name('dashboard');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/cart', [App\Http\Controllers\CartCtrl::class, 'index'])->name('cart');
+    Route::delete('/cart/{id}', [App\Http\Controllers\CartCtrl::class, 'delete'])->name('cart-delete');
 
-Route::get('/dashboard/products', [App\Http\Controllers\DashboardProductCtrl::class, 'index'])
-    ->name('dashboard-products');
-Route::get('/dashboard/product/create', [App\Http\Controllers\DashboardProductCtrl::class, 'create'])
-    ->name('dashboard-product-create');
-Route::get('/dashboard/product/{id}', [App\Http\Controllers\DashboardProductCtrl::class, 'details'])
-    ->name('dashboard-product-details');
+    Route::post('/checkout', [App\Http\Controllers\CheckoutCtrl::class, 'process'])->name('checkout');
 
-Route::get('/dashboard/transactions', [App\Http\Controllers\DashboardTransactionsCtrl::class, 'index'])
-    ->name('dashboard-transactions');
-Route::get('/dashboard/transactions/{id}', [App\Http\Controllers\DashboardTransactionsCtrl::class, 'details'])
-    ->name('dashboard-transactions-details');
+    Route::get('/dashboard', [App\Http\Controllers\DashboardCtrl::class, 'index'])->name('dashboard');
 
-Route::get('/dashboard/settings', [App\Http\Controllers\DashboardAccountSettingsCtrl::class, 'store'])
-    ->name('dashboard-settings-store');
-Route::get('/dashboard/account', [App\Http\Controllers\DashboardAccountSettingsCtrl::class, 'account'])
-    ->name('dashboard-settings-account');
+    Route::get('/dashboard/products', [App\Http\Controllers\DashboardProductCtrl::class, 'index'])
+        ->name('dashboard-products');
+    Route::get('/dashboard/product/create', [App\Http\Controllers\DashboardProductCtrl::class, 'create'])
+        ->name('dashboard-product-create');
+    Route::get('/dashboard/product/{id}', [App\Http\Controllers\DashboardProductCtrl::class, 'details'])
+        ->name('dashboard-product-details');
+
+    Route::get('/dashboard/transactions', [App\Http\Controllers\DashboardTransactionsCtrl::class, 'index'])
+        ->name('dashboard-transactions');
+    Route::get('/dashboard/transactions/{id}', [App\Http\Controllers\DashboardTransactionsCtrl::class, 'details'])
+        ->name('dashboard-transactions-details');
+
+    Route::get('/dashboard/settings', [App\Http\Controllers\DashboardAccountSettingsCtrl::class, 'store'])
+        ->name('dashboard-settings-store');
+    Route::get('/dashboard/account', [App\Http\Controllers\DashboardAccountSettingsCtrl::class, 'account'])
+        ->name('dashboard-settings-account');
+});
 
 Route::prefix('admin')
     ->namespace('App\Http\Controllers\Admin')
+    ->middleware(['auth', 'admin'])
     ->group(function() {
         Route::get('/', [App\Http\Controllers\Admin\AdminDashboardCtrl::class, 'index'])
             ->name('admin-dashboard');
